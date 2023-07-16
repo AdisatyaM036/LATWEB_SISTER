249eeeebce83a4eab7a129261deb52413375c826
AdisatyaM036 <115062213+AdisatyaM036@users.noreply.github.com>
2023-07-16 22:35:30 +0800

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,21 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Data Diri</title>
+    </head>
+    <body>
+        <h1>Data Diri Mahasiswa</h1>
+        <?php
+        $nama = "Komang Adi Satya Mahagangga";
+        $umur = 2115101036;
+        $prodi = "Ilmu Komputer";
+        $email = "satyamahagangga@gmail.com";
+    
+
+        echo "<h2>Nama: $nama</h2>";
+        echo "<p>Umur: $umur</p>";
+        echo "<p>Prodi: $prodi</p>";
+        echo "<p>Email: $email</p>";
+        ?>
+    </body>
+    </html>
